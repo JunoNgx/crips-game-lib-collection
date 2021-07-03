0f2b239ef22cefba5397568c2998df45ada2022b
JunoNgx <juno.ngx@gmail.com>
2021-07-03 12:18:26 +0800

    Handling input

--- docs/orbitdef/main.js
+++ docs/orbitdef/main.js
@@ -22,11 +22,15 @@ options = {
     seed: 2
 };
 
-const ORB_RAD = 50;
+const ORB_RAD = 55;
+const CROSSSHAIR_DISTANCE = 20;
 const MOV_SPD = 0.015;
-const MOV_SPD_SLOWED = 2;
+const MOV_SPD_SLOWED = 0.0075;
+const LONGPRESS_THRESHOLD = 20; // Unit: number of frames; 60 frames equate to one sec
 const FIRE_RATE = 5;
 const SPAWN_RATE = 5;
+
+let lastJustPressed = 0;
 
 /** @type {Vector}[] */
 let asteroids;
@@ -35,6 +39,7 @@ let asteroids;
  * pos: Vector,
  * posAngle: number,
  * gunAngle: number
+ * isFiring: boolean
  * }} */
 let player;
 
@@ -52,7 +57,7 @@ function update() {
         })
 
         // Create player
-        let angle = PI
+        let angle = PI;
 
         player = {
             pos: vec(
@@ -60,7 +65,8 @@ function update() {
                 G_HEIGHT/2 - ORB_RAD*Math.sin(angle)
             ),
             posAngle: angle,
-            gunAngle: PI * 2
+            gunAngle: -PI * 0.25,
+            isFiring: false
         }
     }
 
@@ -82,14 +88,41 @@ function update() {
     arc(EARTH_POS.x - 3, EARTH_POS.y - 1, 3, 3)
     // The island on the bottom left
     arc(EARTH_POS.x - 8, EARTH_POS.y + 9, 2, 3)
+    // Clouds
+    // color("light_black");
+    // arc(EARTH_POS.x - 7, EARTH_POS.y - 7, 3, 5)
+    // arc(EARTH_POS.x - 10, EARTH_POS.y - 4, 3, 3)
+
+    // Hanlding input
+    // Longpress/hold to fire
+    // Shorttap to switch direction
+    let isPassingThreshold = ticks - lastJustPressed > LONGPRESS_THRESHOLD;
+    if (input.isJustPressed) {
+        lastJustPressed = ticks;
+    } else if (input.isPressed) {
+        if (isPassingThreshold) player.isFiring = true;
+    } else if (input.isJustReleased) {
+        if (!isPassingThreshold) player.gunAngle -= PI * 0.5;
+        player.isFiring = false;
+    }
 
     // Updating player
-
-    // color("light_red");
-    player.posAngle -= MOV_SPD;
+    let movSpd = player.isFiring ? MOV_SPD_SLOWED : MOV_SPD;
+    player.posAngle -= movSpd;
     player.pos = vec(
         G_WIDTH/2 + ORB_RAD*Math.cos(player.posAngle),
         G_HEIGHT/2 - ORB_RAD*Math.sin(player.posAngle)
     ),
     char("a", player.pos);
+
+    // Draw the crosshair indicating the firing direction
+    color("light_red");
+    arc(
+        player.pos.x + CROSSSHAIR_DISTANCE*Math.cos(player.gunAngle),
+        player.pos.y + CROSSSHAIR_DISTANCE*Math.sin(player.gunAngle),
+        2,
+        1
+    );
+
+    // text(ticks.toString(10), 0, 0);
 }
