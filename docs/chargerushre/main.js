8eb406848568ce66c19bc79cdae40640235350a5
Juno Nguyen <juno.ngx@gmail.com>
2021-07-11 14:08:58 +0800

    Implement enemies and collision

--- docs/chargerushre/main.js
+++ docs/chargerushre/main.js
@@ -16,8 +16,15 @@ ccllcc
 ccllcc
 ccllcc
 cc  cc
+`,
 `
-
+r    r
+rrrrrr
+rrggrr
+rrggrr
+ r  r
+ r  r
+`
 ];
 
 /**
@@ -26,15 +33,18 @@ cc  cc
 const G = {
     WIDTH: 100,
     HEIGHT: 150,
-    OUTER_BORDER: 10,
+    OUTER_BORDER: 30,
 
     PLAYER_FIRE_RATE: 5,
     PLAYER_GUN_DIST: 3,
 
     FBULLET_SPEED: 5,
 
-    STAR_MIN_VELOCITY: 0.1,
-    STAR_MAX_VELOCITY: 0.5,
+    ENEMY_MIN_BASE_SPEED: 1.5,
+    ENEMY_MAX_BASE_SPEED: 3.0,
+
+    STAR_MIN_VELOCITY: 0.5,
+    STAR_MAX_VELOCITY: 1.0,
 }
 
 options = {
@@ -134,13 +144,37 @@ function update() {
         });
     }
 
+    // Spawner mechanic
+    if (enemies.length === 0) {
+        // enemies = times(9, () => {
+        //     return {
+        //         pos: vec(rnd(G.WIDTH), -rnd(G.OUTER_BORDER)),
+        //         velocity: 0.
+        //     };
+        // });
+
+        let sharedVelocity =
+            rnd(G.ENEMY_MIN_BASE_SPEED, G.ENEMY_MAX_BASE_SPEED)
+            * difficulty;
+
+        for (let i = 1; i < 9; i++) {
+            enemies.push({
+                pos: vec (
+                    rnd(G.WIDTH * 0.1, G.WIDTH * 0.9),
+                    -i * G.HEIGHT/10
+                ),
+                velocity: sharedVelocity
+            })
+        }
+    }
+
     // Star
     stars.forEach((s) => {
         s.pos.y += s.velocity;
 
-        if (s.pos.y > G.HEIGHT + G.OUTER_BORDER) {
+        if (s.pos.y > G.HEIGHT) {
             s.pos = vec(
-                rnd(G.WIDTH)
+                rnd(G.WIDTH),
                 -G.OUTER_BORDER
             )
         }
@@ -167,20 +201,32 @@ function update() {
         player.firingCooldown = G.PLAYER_FIRE_RATE;
         player.isFiringLeft = !player.isFiringLeft;
 
-        particle(player.pos.x + offset, player.pos.y, 4, 1, -PI/2, PI/4);
+        color("black");
+        particle(player.pos.x + offset, player.pos.y, 2, 1, -PI/2, PI/4);
     }
 
     // FBullets
     fBullets.forEach((fb) => {
         fb.pos.y -= G.FBULLET_SPEED;
 
-        color("cyan");
+        color("yellow");
         box(fb.pos, 2, 2);
+    });
+
+    // Enemies
+    remove(enemies, (e) => {
+        e.pos.y += e.velocity;
+        
+        color("black");
+        return (
+            e.pos.y > G.HEIGHT
+            || char("b", e.pos).isColliding.rect.yellow
+        )
     });
 
     remove(fBullets, (fb) => {
         return (
-            fb.pos.y < -G.OUTER_BORDER
+            fb.pos.y < 0
             || box(fb.pos, 2, 2).isColliding.char.b
         );
     });
