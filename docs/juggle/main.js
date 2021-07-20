03f64a1ebc3b65d32339d40b13710a7612180917
Juno Nguyen <juno.ngx@gmail.com>
2021-07-20 20:49:28 +0800

    Implement core gameplay

--- docs/juggle/main.js
+++ docs/juggle/main.js
@@ -2,7 +2,8 @@ title = "JUGGLE";
 
 description = `
 Don't drop.
-Don't overthrow.
+Heal every
+7 hits.
 
 [Tap]
   Catch
@@ -35,8 +36,11 @@ const G = {
 
     COLLISION_CORRECTION: 1,
 
-    BOUNCE_VELOCITY_MIN: 2.0,
+    BOUNCE_VELOCITY_MIN: 2.4,
     BOUNCE_VELOCITY_MAX: 3.0,
+
+    SPAWN_RATE: 300,
+    HEAL_RATE: 10
 };
 
 options = {
@@ -62,6 +66,14 @@ options = {
  */
 
 /**
+ * @typedef {{
+ * spawnCooldown: number,
+ * spawnCount: number,
+ * healHitCooldown: number
+ * }} Progress
+ */
+
+/**
  * @type { Paddle }
  */
 let paddle;
@@ -76,16 +88,43 @@ let balls;
  */
 let hp;
 
+/**
+ * @type { Progress }
+ */
+let progress;
+
 function update() {
     if (!ticks) {
         paddle = { pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.9) };
         balls = [];
 
         hp = 3;
+        progress = {
+            spawnCooldown: G.SPAWN_RATE,
+            spawnCount: 0,
+            healHitCooldown: G.HEAL_RATE
+        }
         // addBall();
     }
 
     if (balls.length === 0) addBall();
+
+    // Progress
+    progress.spawnCooldown--;
+    if (progress.spawnCooldown <= 0) {
+        progress.spawnCooldown = G.SPAWN_RATE;
+        addBall();
+        play("powerUp");
+    }
+    if (progress.healHitCooldown <= 0) {
+        progress.healHitCooldown = G.HEAL_RATE;
+        hp++;
+        play("coin");
+    }
+    if (hp <= 0) {
+        end();
+        play("lucky");
+    }
 
     // HP Bar
     color("red");
@@ -110,16 +149,19 @@ function update() {
         b.pos.add(b.vel);
         b.vel.y += G.GRAVITY;
 
-        // b.pos.wrap(0, G.WIDTH, 0, G.HEIGHT);
         if (b.pos.x < 0) {
             b.vel.x = rnd(G.BALL_SPD_MIN, G.BALL_SPD_MAX);
+
             color("yellow");
             particle(b.pos, 10, 1, 0, PI/4);
+            play("hit");
         }
         if (G.WIDTH < b.pos.x) {
             b.vel.x = -rnd(G.BALL_SPD_MIN, G.BALL_SPD_MAX);
+
             color("yellow");
             particle(b.pos, 10, 1, -PI, PI/4);
+            play("hit");
         }
 
         color("cyan");
@@ -131,9 +173,22 @@ function update() {
             b.vel.y =
                 -rnd(G.BOUNCE_VELOCITY_MIN, G.BOUNCE_VELOCITY_MAX);
 
+            progress.healHitCooldown--;
+
             color("yellow");
             particle(b.pos, 10, 1, -PI/2, PI/4);
+            play("laser");
         }
+
+        const isDropped = b.pos.y > G.HEIGHT;
+        if (isDropped) {
+            hp--;
+            color("yellow");
+            particle(b.pos, 20, 4, -PI/2, PI/4);
+            play("explosion");
+        }
+
+        return isDropped;
     });
 
     function addBall() {
@@ -145,5 +200,10 @@ function update() {
                 0
             )
         })
+
+        progress.spawnCount++;
+
+        color("yellow");
+        particle(vec(G.WIDTH * 0.5, G.WIDTH * 0.3));
     }
 }
