9a1320788256ea4ce1e7999cfd943f9071d0f614
Juno Nguyen <juno.ngx@gmail.com>
2021-07-20 20:22:42 +0800

    Implement basic gameplay

--- docs/juggle/main.js
+++ docs/juggle/main.js
@@ -15,20 +15,25 @@ const G = {
     WIDTH: 100,
     HEIGHT: 120,
 
-    GRAVITY: 1,
+    GRAVITY: 0.05,
 
-    PADDLE_LENGTH: 10,
+    PADDLE_LENGTH: 15,
     PADDLE_HEIGHT: 1,
 
     BALL_RADIUS: 2,
-    BALL_SPD_HORIZONTAL: 2
+    BALL_SPD_MIN: 0.8,
+    BALL_SPD_MAX: 1.2,
+
+    COLLISION_CORRECTION: 1,
+
+    BOUNCE_VELOCITY_MIN: 2.0,
+    BOUNCE_VELOCITY_MAX: 3.0,
 };
 
 options = {
     viewSize: {x: G.WIDTH, y: G.HEIGHT},
     theme: "simple",
     isDrawingParticleFront: true,
-    isDrawingScoreFront: true,
     isPlayingBgm: false,
     isReplayEnabled: false,
     isCapturing: false
@@ -57,6 +62,11 @@ let paddle;
  */
 let balls;
 
+/**
+ * @type { number }
+ */
+let hp;
+
 function update() {
     if (!ticks) {
         paddle = { pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.9) };
@@ -67,7 +77,7 @@ function update() {
     paddle.pos.x = input.pos.x;
     paddle.pos.x = clamp(paddle.pos.x, 0, G.WIDTH);
 
-    color("cyan");
+    color("light_green");
     rect(
         paddle.pos.x - G.PADDLE_LENGTH/2,
         paddle.pos.y - G.PADDLE_HEIGHT/2,
@@ -75,12 +85,42 @@ function update() {
         G.PADDLE_HEIGHT
     );
 
+    remove(balls, (b) => {
+        b.pos.add(b.vel);
+        b.vel.y += G.GRAVITY;
+
+        // b.pos.wrap(0, G.WIDTH, 0, G.HEIGHT);
+        if (b.pos.x < 0) {
+            b.vel.x = rnd(G.BALL_SPD_MIN, G.BALL_SPD_MAX);
+            color("light_green");
+            particle(b.pos, 10, 1, 0, PI/4);
+        }
+        if (G.WIDTH < b.pos.x) {
+            b.vel.x = -rnd(G.BALL_SPD_MIN, G.BALL_SPD_MAX);
+            color("light_green");
+            particle(b.pos, 10, 1, -PI, PI/4);
+        }
+
+        color("cyan");
+        const collidingWithPaddle =
+            arc(b.pos, G.BALL_RADIUS, 1).isColliding.rect.light_green;
+
+        if (collidingWithPaddle) {
+            b.pos.y -= G.COLLISION_CORRECTION;
+            b.vel.y =
+                -rnd(G.BOUNCE_VELOCITY_MIN, G.BOUNCE_VELOCITY_MAX);
+
+            color("light_green");
+            particle(b.pos, 10, 1, -PI/2, PI/4);
+        }
+    });
+
     function addBall() {
         balls.push({
             pos: vec(G.WIDTH * 0.5, G.WIDTH * 0.3),
             vel: vec(rnd() > 0
-                ? G.BALL_SPD_HORIZONTAL
-                : -G.BALL_SPD_HORIZONTAL,
+                ? G.BALL_SPD_MIN
+                : -G.BALL_SPD_MAX,
                 0
             )
         })
