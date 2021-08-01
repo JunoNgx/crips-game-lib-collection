1526c0bd1e238f15096f408ff130c41f53389309
Juno Nguyen <juno.ngx@gmail.com>
2021-08-01 11:52:41 +0800

    Update template

--- docs/_template/main.js
+++ docs/_template/main.js
@@ -3,11 +3,32 @@ title = "";
 description = `
 `;
 
+const G = {
+    WIDTH: 100,
+    HEIGHT: 100,
+}
+
 characters = [];
 
-options = {};
+options = {
+    viewSize: {x: G.WIDTH, y: G.HEIGHT},
+    theme: "simple",
+    isDrawingParticleFront: true,
+    isDrawingScoreFront: true,
+    // isPlayingBgm: true,
+    // isReplayEnabled: true,
+    // isCapturing: true,
+    // isCapturingGameCanvasOnly: true,
+    // captureCanvasScale: 2,
+    seed: 1
+};
+
+/** @typedef { {pos: Vector} } Player */
+/** @type { Player } */
+let player;
 
 function update() {
-  if (!ticks) {
-  }
+    if (!ticks) {
+
+    }
 }
