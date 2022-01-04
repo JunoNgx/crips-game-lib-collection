3b6ff6e8bbe70dd4b6b334c3dedcf1ff4faa05a7
Juno Nguyen <juno.ngx@gmail.com>
2022-01-05 02:06:35 +0800

    Game7: remove semicolons

--- docs/game7/main.js
+++ docs/game7/main.js
@@ -26,7 +26,7 @@ ccrcc
  ccc
   c
 `
-];
+]
 
 const G = {
     WIDTH: 92,
@@ -51,18 +51,18 @@ options = {
     // isCapturingGameCanvasOnly: true,
     // captureCanvasScale: 2,
     seed: 1
-};
+}
 
 /** @type { {pos: Vector, angle: number, isRotating: boolean, isRotatingRight: boolean} } */
 let cannon
 /** @type { {pos: Vector, vel: Vector, angle: number} } */
 let missile
 /** @type { {pos: Vector, vel: Vector} [] }*/
-let enemies;
+let enemies
 /** @type { {pos: Vector, lifetime: number} [] } */
-let explosions;
+let explosions
 /** @type { number } */
-let spawnCooldown;
+let spawnCooldown
 
 function update() {
     if (!ticks) {
@@ -72,26 +72,26 @@ function update() {
             isRotating: true,
             isRotatingRight: true
         }
-        missile = null;
-        enemies = [];
-        explosions = [];
-        spawnCooldown = G.SPAWN_RATE_BASE;
+        missile = null
+        enemies = []
+        explosions = []
+        spawnCooldown = G.SPAWN_RATE_BASE
     }
 
     // Mechanics
-    spawnCooldown--;
+    spawnCooldown--
     if (spawnCooldown <= 0) {
 
-        let destVec = vec(G.WIDTH * 0.5 + rnd(-5, 5), G.HEIGHT * 0.82);
+        let destVec = vec(G.WIDTH * 0.5 + rnd(-5, 5), G.HEIGHT * 0.82)
         let initVec = vec(
             rnd(G.WIDTH * -0.2, G.WIDTH * 1.2),
             rnd(G.HEIGHT * - 0.2, G.HEIGHT * 0.5)
-        );
+        )
         while (initVec.isInRect(0, 0, G.WIDTH, G.HEIGHT)) {
             initVec = vec(
                 rnd(G.WIDTH * -0.2, G.WIDTH * 1.2),
                 rnd(G.HEIGHT * - 0.2, G.HEIGHT * 0.5)
-            );
+            )
         } 
 
         enemies.push({
@@ -100,7 +100,7 @@ function update() {
                 .rotate(initVec.angleTo(destVec))
         })
 
-        spawnCooldown = G.SPAWN_RATE_BASE - difficulty*20;
+        spawnCooldown = G.SPAWN_RATE_BASE - difficulty*20
     }
     
     if (input.isJustPressed) {
@@ -108,7 +108,7 @@ function update() {
         if (missile === null) { // Fire a missile
 
             const initPos = vec(cannon.pos.x, cannon.pos.y)
-                .addWithAngle(cannon.angle, G.BARREL_LENGTH);
+                .addWithAngle(cannon.angle, G.BARREL_LENGTH)
     
             missile = {
                 pos: initPos,
@@ -116,20 +116,20 @@ function update() {
                 angle: cannon.angle
             }
     
-            color("yellow");
-            particle(initPos, 7, 2, cannon.angle, PI/4);
-            // play();
+            color("yellow")
+            particle(initPos, 7, 2, cannon.angle, PI/4)
+            // play()
 
         } else { // Else detonate the missile
 
             explosions.push({
                 pos: vec(missile.pos.x, missile.pos.y),
                 lifetime: 0
-            });
-            color("red");
-            particle(missile.pos, 20, 3);
+            })
+            color("red")
+            particle(missile.pos, 20, 3)
             missile = null
-            // play("select");
+            // play("select")
 
         }
     }
@@ -137,9 +137,9 @@ function update() {
     // Entities
 
     // The ground
-    color("light_black");
-    rect(G.WIDTH * 0.0, G.HEIGHT * 0.930, G.WIDTH * 1.0, G.HEIGHT * 0.075);
-    rect(G.WIDTH * 0.4, G.HEIGHT * 0.875, G.WIDTH * 0.2, G.HEIGHT * 0.075); 
+    color("light_black")
+    rect(G.WIDTH * 0.0, G.HEIGHT * 0.930, G.WIDTH * 1.0, G.HEIGHT * 0.075)
+    rect(G.WIDTH * 0.4, G.HEIGHT * 0.875, G.WIDTH * 0.2, G.HEIGHT * 0.075) 
 
     // Cannon
     if (!missile) {
@@ -152,10 +152,10 @@ function update() {
         }
     }
     // Aiming line
-    color("green");
+    color("green")
     if (!missile) {
         color("green")
-        bar(cannon.pos, 30, 1, cannon.angle, 0.01);
+        bar(cannon.pos, 30, 1, cannon.angle, 0.01)
     }
     // The actual cannon sprite
     color("black")
@@ -176,28 +176,28 @@ function update() {
     }
 
     remove(explosions, (e) => {
-        e.lifetime++;
-        const radius = sin(e.lifetime * 0.2) * G.EXPLOSION_BASE_RADIUS;
+        e.lifetime++
+        const radius = sin(e.lifetime * 0.2) * G.EXPLOSION_BASE_RADIUS
 
-        color("red");
-        arc(e.pos, radius);
-        return (radius < 0);
-    });
+        color("red")
+        arc(e.pos, radius)
+        return (radius < 0)
+    })
 
     remove(enemies, (e) => {
-        e.pos.add(e.vel);
+        e.pos.add(e.vel)
 
-        color("black");
+        color("black")
         const isCollidingWithExplosion = char("b", e.pos)
-            .isColliding.rect.red;
+            .isColliding.rect.red
 
         const isCollidingWithPlayer = char("b", e.pos)
             .isColliding.char.a
 
         if (isCollidingWithExplosion) {
-            color("purple");
-            particle(e.pos, 10, 2);
-            // play("hit");
+            color("purple")
+            particle(e.pos, 10, 2)
+            // play("hit")
         }
 
         if (isCollidingWithPlayer) {
@@ -207,6 +207,6 @@ function update() {
             text("x", cannon.pos)
         }
 
-        return (isCollidingWithExplosion);
-    });
+        return (isCollidingWithExplosion)
+    })
 }
